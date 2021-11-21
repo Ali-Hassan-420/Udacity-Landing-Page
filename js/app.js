a0226cdd13e36d0faf898b3d252366267d7888dc
Ali-Hassan-420 <83880333+Ali-Hassan-420@users.noreply.github.com>
2021-11-21 12:44:20 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -18,22 +18,27 @@
  * Great to have comments before crucial code sections within the procedure.
 */
 
-
 /**
  * Define Global Variables
  * 
 */
 
 const nav = document.getElementsByClassName('navbar__menu');
+
 const nav_list = document.getElementById('navbar__list');
 
 const section1 = document.getElementById('section1');
+
 const section2 = document.getElementById('section2');
+
 const section3 = document.getElementById('section3');
 
 let allSections = document.querySelectorAll("section")
 
 const lander = document.getElementById("land")
+
+var goToTopElement = document.getElementById("top")
+
 
 /**
  * End Global Variables
@@ -45,85 +50,12 @@ function landingPage () {
     lander.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"})
 }
 
+
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
 */
-
-// build the nav
-
-const navBuilder = () => {
-
-    let navUI = '';
-
-    allSections.forEach(section => {
-
-        const sectionID = section.id;
-        const sectionDataNav = section.dataset.nav;
-
-        navUI += `<li><a class="menu__link" id="alpha${sectionID}"href="#${sectionID}">${sectionDataNav}</a></li>`
-    });
-
-    //append all elements the navigation
-    nav_list.innerHTML = navUI;
-};
-
-navBuilder()
-
-
-// Add class 'active' to section when near top of viewport
-
-for (var i=0; i<100; i++) {
-    function sectionInViewPort(allsections) {
-        let sPosition = allsections.getBoundingClientRect();
-        return sPosition.top >= -400 &&  sPosition.top <= 150;
-    }
-    
-    
-    document.addEventListener("scroll", function setActiveState() {
-        for (let i = 0; i < allSections.length; i++) {
-          if (sectionInViewPort(allSections[i])) {
-            if (!allSections[i].classList.contains("active")) {
-              allSections[i].classList.add("active");
-            }
-          } else {
-            allSections[i].classList.remove("active");
-          }
-        }
-    })
-}
-
-// Scroll to anchor ID using scrollTO event
-
-const scrolling = () => {
-
-    const links = document.querySelectorAll('.navbar__menu a');
-    links.forEach(link => {
-        link.addEventListener('click', () => {
-            for(i = 0 ; i<sections ; i++){
-                sections[i].addEventListener("click",sectionScroll(link));
-            }
-        });
-    });
-
-};
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-
-//scroll to top
-
-var goToTopElement = document.getElementById("top")
-
-goToTopElement.addEventListener("click", landingPage)
-
-
-//**activeNavBar
 
 function activeNavBar () {
     if (section1.classList.contains("active")) {
@@ -148,5 +80,90 @@ function activeNavBar () {
         alphasection1.classList.remove("menu__active");
     }
 }
+
+// build the nav
+
+function navigationBuilder () {
+
+    let navigationUI = '';
+
+    for(section of allSections) {
+
+        //declaring the ID and the dataset to connect between sections and navBars
+        let sectionID = section.id;
+        let sectionDataNav = section.dataset.nav;
+
+        //building the nav template
+        navigationUI += `<li><a class="menu__link" id="alpha${sectionID}"href="#${sectionID}">${sectionDataNav}</a></li>`
+    };
+
+    //append all elements the navigation
+    nav_list.innerHTML = navigationUI;
+};
+
+//executing the navigation builder function
+navigationBuilder()
+
+
+// Add class 'active' to section when near top of viewport
+
+for (var i=0; i<100; i++) {
+    function sectionInViewPort(allsections) {
+        let sPosition = allsections.getBoundingClientRect();
+        return sPosition.top >= -400 &&  sPosition.top <= 150;
+    }
+    
+    
+    document.addEventListener("scroll", function setActiveState() {
+        for (let i = 0; i < allSections.length; i++) {
+          if (sectionInViewPort(allSections[i])) {
+            if (!allSections[i].classList.contains("active")) {
+              allSections[i].classList.add("active");
+            }
+          } else {
+            allSections[i].classList.remove("active");
+          }
+        }
+    })
+}
+
+
+// Scroll to anchor ID using scrollTO event
+
+
+alphasection1.addEventListener("click", () => {
+    section1.scrollIntoView()
+    console.log("scrolled into section 1")
+})
+
+alphasection2.addEventListener("click", () => {
+    section2.scrollIntoView()
+    console.log("scrolled into section 2")
+})
+
+alphasection3.addEventListener("click", () => {
+    section3.scrollIntoView()
+    console.log("scrolled into section 3")
+})
+
+alphasection4.addEventListener("click", () => {
+    section4.scrollIntoView()
+    console.log("scrolled into section 4")
+})
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Scroll to top section on link click
+
+
+goToTopElement.addEventListener("click", landingPage)
+
+
+// Set sections as active
 
 window.addEventListener("scroll", activeNavBar)
